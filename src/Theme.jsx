35157cda40bb4f0b2d325dce441da9f763aeb214
fofalafofolle <ncirfarah7@gmail.com>
2025-10-24 16:12:18 +0100

    Exercice2 commit

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,21 +1,3 @@
-/*exercice 1 */
-/*
-import React from "react";
-import Time from "./Time";
-
-function App() {
-  return (
-    <div>
-      <h1>Timer</h1>
-      <Time />
-    </div>
-  );
-}
-
-export default App;
-*/
-
-/*exercice 2 */
 import React, { useState, createContext, useContext } from "react";
 
 const ThemeContext = createContext();
@@ -36,19 +18,8 @@ function ThemeProvider({ children }) {
 function ThemeToggle() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  const pageStyle = {
-    backgroundColor: theme === "light" ? "#fff" : "#333",
-    color: theme === "light" ? "#000" : "#fff",
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    transition: "all 0.3s ease",
-  };
-
   return (
-    <div style={pageStyle}>
+    <div>
       <p>Thème actuel : {theme}</p>
       <button onClick={toggleTheme}>Changer le thème</button>
     </div>
